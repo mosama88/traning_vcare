9aa89c5acdbf065fc0906e8c0f684a6b6c761d65
mosama88 <mosama88@hotmail>
2025-04-23 15:52:53 +0200

    CountryResource

--- app/Http/Controllers/Dashboard/CountryController.php
+++ app/Http/Controllers/Dashboard/CountryController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\Country;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CountryResource;
+use App\Http\Requests\Dashboard\CountryRequest;
 
 class CountryController extends Controller
 {
-    public function index(){
-        return response()->json(['donew']);
+    public function index()
+    {
+        $country = Country::paginate(10);
+        return response()->json([CountryResource::collection($country), "Data Retrived Successfully", 200]);
+    }
+
+
+    public function show(Country $country)
+    {
+        return response()->json([new CountryResource($country), "Data Retrived Successfully", 200]);
+    }
+
+
+    public function store(CountryRequest $request)
+    {
+
+        $country = Country::create($this->mapRequestToCulomns($request->validated()));
+
+        return response()->json(['data' => new CountryResource($country), "Data Inserted Successfully", 200]);
+    }
+
+
+    public function update(CountryRequest $request, Country $country)
+    {
+        $country->update($this->mapRequestToCulomns($request->validated()));
+
+        return response()->json([new CountryResource($country), "Data Updated Successfully", 200]);
+    }
+
+
+    public function destroy(Country $country)
+    {
+        $country->delete();
+
+        return response()->json([new CountryResource($country), "Data Deleted Successfully", 200]);
+    }
+
+
+    private function mapRequestToCulomns($data)
+    {
+        return ['name' => $data['country_name']];
     }
 }
--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -15,7 +15,7 @@ class CountryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'Country Number' => $this->id,
+            'Country ID' => $this->id,
             'Country Name' => $this->name,
         ];
     }
